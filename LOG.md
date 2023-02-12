2c5f0d85d4a211606fadd6ebdc9b3709d4c75ab7
Behzaad Pathan <4355.nav.stkabir@gmail.com>
2023-02-12 03:57:45 -0500

    Rules in README
    -wrote the rules of the game
    -have a basic plan to go move forward with the project
    -gets a random movie from the list everytime

---
+++ LOG.md
@@ -0,0 +1,5 @@
+Log 1(11/02/2023):\
+My initial idea is to store an already made list with a bunch
+of bollywood movie names and the user can play it in terminal.
+-I am getting a random movie from list.
+-
--- bollywood.py
+++ bollywood.py
@@ -1,4 +1,9 @@
-# Bollywood
+# Bollywood in terminal
 import random
 
+index = random.randint(0,20)
+file = open('movies.txt','r')
+l = file.readlines()
+selectedMovie = l[index].strip('\n')
 
+print(selectedMovie) #checking to see if a random movie is being printed from the selected list
