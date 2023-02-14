b912b76a98dfa72878d355dcfb686e560f0f5ef3
Behzaad Pathan <4355.nav.stkabir@gmail.com>
2023-02-14 02:26:12 -0500

    functions to read correct input and display already used characters

--- bollywood.py
+++ bollywood.py
@@ -1,17 +1,51 @@
 # Bollywood in terminal
 import random
+from colorama import Fore, Back, Style
 
 index = random.randint(0, 20)
 file = open('movies.txt', 'r')
 l = file.readlines()
 selectedMovie = l[index].strip('\n')
+count = 0
+chances = 0
+used = "AEIOU:"  # including ':'
+
+print(Fore.BLUE + "Hey, let's play Bollywood!")
+print(Fore.RED + "BOLLYWOOD" + Style.RESET_ALL)
+
+# movie with vowels
+def movieName():
+    for char in selectedMovie:
+        if char in used:
+            print(char, end='')
+        elif char == " ":
+            print(" ", end='')
+        else:
+            print("_", end='')
+
+def askInput(guess=None):
+    global used
+    while True:
+        user_input = input("\nPlease guess a letter or a number: ")
+        if len(user_input) == 1 and (user_input.isalpha() or user_input.isdigit()) and user_input.upper() not in used:
+            used += user_input.upper()
+            user_input = guess
+            print(used)
+            break
+        else:
+            print("Invalid input, please enter only an unused letter or a number.")
+            movieName()
+
+#def checkGuess():
+
+movieName()
+askInput()
 
 print(selectedMovie)  # checking to see if a random movie is being printed from the selected list
 # doneTODO: implement finding vowels and then provide the vowels to the player for easy mode
 # print(*[i for i in selectedMovie if i in 'aeiou']) failed attempt
 
-count = 0
-vowels = "AEIOU:"  # including ':'
+
 for i in range(0, len(selectedMovie)):
     if selectedMovie[i] != ' ':
         count = count + 1
@@ -26,11 +60,5 @@ for char in selectedMovie:
         print("_", end="")
 print("")
 
-# movie with vowels
-for char in selectedMovie:
-    if char in vowels:
-        print(char, end='')
-    elif char == " ":
-        print(" ", end='')
-    else:
-        print("_", end='')
+
+
